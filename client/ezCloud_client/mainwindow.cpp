554577acdb78209a44978dd7f9b7204b89a221e9
yrralh <295979082@QQ.COM>
2020-04-01 16:00:23 +0800

    hj heavy

--- client/ezCloud_client/mainwindow.cpp
+++ client/ezCloud_client/mainwindow.cpp
@@ -11,9 +11,16 @@ MainWindow::MainWindow(QWidget *parent)
     connect(&clientSocket,&ClientSocket::error,this,&MainWindow::errorHandler,Qt::QueuedConnection);
     connect(&clientSocket,&ClientSocket::changeToDownload,this,&MainWindow::changeToDownload,Qt::QueuedConnection);
 
+    connect(&downloadUI.tbv_download, SIGNAL(clicked(const QModelIndex &)), this, SLOT(tableViewClicked_download(const QModelIndex &)));
+    connect(&uploadUI.tbv_upload, SIGNAL(clicked(const QModelIndex &)), this, SLOT(tableViewClicked_upload(const QModelIndex &)));
+
     clientSocket.moveToThreadAll(&socketThread);
+
     socketThread.start();
-    loginUI.show();
+    //loginUI.show();
+    uploadUI.show();
+    this->dbInit();
+    this->uploadTableRefresh(test, 2);
 }
 
 MainWindow::~MainWindow()
@@ -55,4 +62,150 @@ void MainWindow::changeToDownload()
 void MainWindow::changeToUpload()
 {
 
+}
+
+
+
+
+
+
+void MainWindow::dbInit()
+{
+    this->db.close();
+    this->db = QSqlDatabase::addDatabase("QSQLITE");
+    this->db.setDatabaseName(qstr_dbname);
+    if (!this->db.open()) {
+        qDebug() << "Error: tableViewInit(QTableView* tbv) Failed to connect database." << this->db.lastError();
+    }else{
+        qDebug() << "tableViewInit(QTableView* tbv) Succeed to connect database." ;
+    }
+
+    return;
+}
+
+void MainWindow::downloadTableRefresh(std::string p_str[], const int rowNum)
+{
+    QSqlQuery sql_query_temp;
+    QString cmd;
+
+    cmd = "drop table " + qstr_tbname_download;
+    qDebug() << cmd;
+    if(!sql_query_temp.exec(cmd)){
+        qDebug() << "Error: MainWindow::downloadTableRefresh Fail to delete table."<< sql_query_temp.lastError();
+    }else{
+        qDebug() << "MainWindow::downloadTableRefresh Table deleted!";
+    }
+
+    cmd = "create table " + qstr_tbname_download + "(fileLength int, fileName text)";
+    qDebug() << cmd;
+    if(!sql_query_temp.exec(cmd)){
+        qDebug() << "Error: MainWindow::downloadTableRefresh Fail to create table."<< sql_query_temp.lastError();
+    }else{
+        qDebug() << "MainWindow::downloadTableRefresh Table created!";
+    }
+
+    int rowLength = 3;
+    QString qstr_tmp0;
+    QString qstr_tmp1;
+    for(int i = 0; i < rowNum; i++)
+    {
+        qstr_tmp0 = QString::fromStdString(p_str[i*rowLength + 0]);
+        qstr_tmp1 = "'" + QString::fromStdString(p_str[i*rowLength + 1]) + "'";
+        cmd = "insert into " + qstr_tbname_download + " values(" + qstr_tmp0 + "," + qstr_tmp1 + ")";
+        qDebug() << cmd;
+        if(!sql_query_temp.exec(cmd)){
+            qDebug() << "Error: MainWindow::downloadTableRefresh Fail to insert."<< sql_query_temp.lastError();
+        }else{
+            qDebug() << "MainWindow::downloadTableRefresh insert success!";
+        }
+    }
+
+
+
+    if(mdl_download != nullptr)
+    {
+        delete mdl_download;
+    }
+    mdl_download = new QSqlTableModel(this, db);
+    mdl_download->setTable(qstr_tbname_download);
+    mdl_download->setEditStrategy(QSqlTableModel::OnRowChange);
+
+    mdl_download->select();
+    downloadUI.tbv_download.setEditTriggers(QAbstractItemView::NoEditTriggers);
+    downloadUI.tbv_download.setModel(mdl_download);
+    downloadUI.tbv_download.setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    return;
+}
+
+void MainWindow::uploadTableRefresh(std::string p_str[], const int rowNum)
+{
+    QSqlQuery sql_query_temp;
+    QString cmd;
+
+    cmd = "drop table " + qstr_tbname_upload;
+    qDebug() << cmd;
+    if(!sql_query_temp.exec(cmd)){
+        qDebug() << "Error: MainWindow::uploadTableRefresh Fail to delete table."<< sql_query_temp.lastError();
+    }else{
+        qDebug() << "MainWindow::uploadTableRefresh Table deleted!";
+    }
+
+    cmd = "create table " + qstr_tbname_upload + "(fileLength int, fileName text)";
+    qDebug() << cmd;
+    if(!sql_query_temp.exec(cmd)){
+        qDebug() << "Error: MainWindow::uploadTableRefresh Fail to create table."<< sql_query_temp.lastError();
+    }else{
+        qDebug() << "MainWindow::uploadTableRefresh Table created!";
+    }
+
+    int rowLength = 3;
+    QString qstr_tmp0;
+    QString qstr_tmp1;
+    for(int i = 0; i < rowNum; i++)
+    {
+        qstr_tmp0 = QString::fromStdString(p_str[i*rowLength + 0]);
+        qstr_tmp1 = "'" + QString::fromStdString(p_str[i*rowLength + 1]) + "'";
+
+        cmd = "insert into " + qstr_tbname_upload + " values(" + qstr_tmp0 + "," + qstr_tmp1 + ")";
+
+        qDebug() << cmd;
+        if(!sql_query_temp.exec(cmd)){
+            qDebug() << "Error: MainWindow::uploadTableRefresh Fail to insert."<< sql_query_temp.lastError();
+        }else{
+            qDebug() << "MainWindow::uploadTableRefresh insert success!";
+        }
+    }
+
+
+
+    if(mdl_upload != nullptr)
+    {
+        delete mdl_upload;
+    }
+    mdl_upload = new QSqlTableModel(this, db);
+    mdl_upload->setTable(qstr_tbname_upload);
+    mdl_upload->setEditStrategy(QSqlTableModel::OnRowChange);
+
+    mdl_upload->select();
+    uploadUI.tbv_upload.setEditTriggers(QAbstractItemView::NoEditTriggers);
+    uploadUI.tbv_upload.setModel(mdl_upload);
+    uploadUI.tbv_upload.setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    return;
+}
+
+
+void MainWindow::tableViewClicked_download(const QModelIndex& index)
+{
+    currentRowNum_download = index.row();
+    rcd_current_download = mdl_download->record(currentRowNum_download);
+    qDebug() << currentRowNum_download;
+}
+
+void MainWindow::tableViewClicked_upload(const QModelIndex& index)
+{
+    currentRowNum_upload = index.row();
+    rcd_current_upload = mdl_upload->record(currentRowNum_upload);
+    qDebug() << currentRowNum_upload;
 }
--- client/ezCloud_client/mainwindow.h
+++ client/ezCloud_client/mainwindow.h
@@ -5,12 +5,39 @@
 #include <QMessageBox>
 #include <QDialog>
 #include <QThread>
+#include <string>
+#include <QTableView>
+#include <QtSql/QSqlRecord>
+#include <QSqlQueryModel>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QSqlRecord>
 #include "ui_header.h"
 #include "clientsocket.h"
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    std::string test[6] = {"2", "name1", "1", "3", "name1", "2"};
+
+    QString qstr_dbname = "tmpDB";
+    QString qstr_tbname_upload = "up";
+    QString qstr_tbname_download = "down";
+    QSqlDatabase db;
+    QSqlTableModel* mdl_download = nullptr;
+    QSqlTableModel* mdl_upload = nullptr;
+
+    int currentRowNum_download;
+    int currentRowNum_upload;
+    QSqlRecord rcd_current_download;
+    QSqlRecord rcd_current_upload;
+
+    void dbInit();
+    void downloadTableRefresh(std::string[], const int);
+    void uploadTableRefresh(std::string[], const int);
 
 public:
     UIpack_as_widget_login loginUI;
@@ -27,6 +54,9 @@ public:
 
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void tableViewClicked_download(const QModelIndex& index);
+    void tableViewClicked_upload(const QModelIndex& index);
 
 };
 #endif // MAINWINDOW_H
